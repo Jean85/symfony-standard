90bbb7a7d5459b23ef0551e5c1d5843aa0ef1017
Alessandro Lai <alessandro.lai@facile.it>
2016-03-15 10:46:38 +0100

    Configured with FOSUserBundle, 2 bundles, 2 ORMs and SQLite

---
+++ src/BisBundle/BisBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BisBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BisBundle extends Bundle
+{
+}
---
+++ src/BisBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BisBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BisBundle:Default:index.html.twig');
+    }
+}
